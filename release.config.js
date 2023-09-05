597abb25a7fd4c20e1c3c0df125c28c83aed6033
DashnyamB <dashnyam.bayarsaikhan@erin.systems>
2023-09-05 18:50:00 +0800

    fix: aaa

--- release.config.js
+++ release.config.js
@@ -4,19 +4,15 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
 
-    [
-      '@semantic-release/npm',
-      { npmPublish: false, tarballDir: 'dist', pkgRoot: '.' },
-    ],
-    '@semantic-release/github',
+    ['@semantic-release/npm', { npmPublish: false }],
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'package-lock.json'],
         message:
           /* eslint-disable no-template-curly-in-string */
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
+    '@semantic-release/github',
   ],
 };

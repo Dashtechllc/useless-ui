4605ffe911a3490a46e384ea8e503b0e9f63684b
DashnyamB <lord_dashka277@yahoo.com>
2023-09-06 19:08:21 +0800

    fix: common

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
+import common from '@rollup/plugin-commonjs'
 
 export default [
   {
@@ -16,6 +17,7 @@ export default [
     ],
 
     plugins: [
+      common(),
       babel({
         exclude: 'node_modules/**',
       }),

e0fc5fc494d5feb2e2bdccdec7e2ca81df331bff
DashnyamB <lord_dashka277@yahoo.com>
2023-09-06 17:20:31 +0800

    fix: fix module

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default [
   {
@@ -12,13 +13,14 @@ export default [
         file: 'dist/index.es.js',
         format: 'es',
         exports: 'named',
+        sourcemap: true,
       },
     ],
 
     plugins: [
+      commonjs(),
       babel({
         exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
       }),
       external(),
       resolve({
